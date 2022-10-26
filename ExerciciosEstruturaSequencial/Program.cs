7edf2d0cd7ab223bc09e536580d26c4fdc7e82df
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-10-26 20:14:17 -0300

    feat: primeiro exercicio concluido

--- ExerciciosEstruturaSequencial/Program.cs
+++ ExerciciosEstruturaSequencial/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ExeciciosEstruturaSequencial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercicio 01
+            Console.WriteLine("Digite um número inteiro:");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro número inteiro ao qual será somado ao anterior:");
+            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Resultado: {n1 + n2}");
+
+        }
+    }
+}
