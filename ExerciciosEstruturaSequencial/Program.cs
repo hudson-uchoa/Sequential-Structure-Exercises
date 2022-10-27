a257319a010530b32a9e50b076d8cf89a7c80853
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-10-26 21:14:24 -0300

    feat: terceiro e quarto exercicio concluidos.

--- ExerciciosEstruturaSequencial/Program.cs
+++ ExerciciosEstruturaSequencial/Program.cs
@@ -19,6 +19,27 @@ namespace ExeciciosEstruturaSequencial
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             double pi = 3.14159;
             Console.WriteLine("Resultado: "+ (pi * Math.Pow(raio, 2.0)).ToString("F4", CultureInfo.InvariantCulture));
+
+            //Exercicio 03
+
+            Console.WriteLine("Digite 4 números inteiros em ordem sequencial na mesma linha (A, B, C e D):");
+            string[] nums = Console.ReadLine().Split(' ');
+            int A = int.Parse(nums[0]);
+            int B = int.Parse(nums[1]);
+            int C = int.Parse(nums[2]); 
+            int D = int.Parse(nums[3]);
+            Console.WriteLine($"A diferença do produto de A e B pelo produto de C e D é: {(A * B) - (C * D)}");
+
+            //Exercicio 04
+
+            Console.WriteLine("Qual o número do funcionario?");
+            int employee = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quantas horas o funcionario acima trabalhou?");
+            int hours = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quanto esse funcionario recebe por horas trabalhadas?");
+            double hourlyPaycheck = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Número do funcionario: " + employee+"\nSalário do funcionario: U$" + (hours * hourlyPaycheck).ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
