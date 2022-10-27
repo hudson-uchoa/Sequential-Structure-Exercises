af31ff52d2df9bd15072b65022ce6cd02b2ded17
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-10-27 20:15:49 -0300

    feat: quinto exercicio concluido

--- ExerciciosEstruturaSequencial/Program.cs
+++ ExerciciosEstruturaSequencial/Program.cs
@@ -42,23 +42,34 @@ namespace ExeciciosEstruturaSequencial
 
             //Console.WriteLine("Número do funcionario: " + employee + "\nSalário do funcionario: U$" + (hours * hourlyPaycheck).ToString("F2", CultureInfo.InvariantCulture));
 
-            //Execicio 05
+            ////Execicio 05
 
-            Console.WriteLine("Digite o código da primeira peça:");
-            int part1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite quantas desta peça irá precisar:");
-            int numOfPart1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Qual o valor unitário dessa peça?");
-            double priceOfPart1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Console.WriteLine("Digite o código da primeira peça:");
+            //int part1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite quantas desta peça irá precisar:");
+            //int numOfPart1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Qual o valor unitário dessa peça?");
+            //double priceOfPart1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine("Digite o código da segunda peça:");
-            int part2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite quantas desta peça irá precisar:");
-            int numOfPart2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Qual o valor unitário dessa peça?");
-            double priceOfPart2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Console.WriteLine("Digite o código da segunda peça:");
+            //int part2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite quantas desta peça irá precisar:");
+            //int numOfPart2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Qual o valor unitário dessa peça?");
+            //double priceOfPart2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine("VALOR TOTAL A PAGAR: R$" + ((priceOfPart1 * numOfPart1) + (priceOfPart2 * numOfPart2)).ToString("F2", CultureInfo.InvariantCulture));
+            //Console.WriteLine("VALOR TOTAL A PAGAR: R$" + ((priceOfPart1 * numOfPart1) + (priceOfPart2 * numOfPart2)).ToString("F2", CultureInfo.InvariantCulture));
+
+            //Exercicio 06
+
+            Console.WriteLine("Digite o valor de A (tem que ser um double):");
+            double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite o valor de B (tem que ser um double):");
+            double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite o valor de C (tem que ser um double):");
+            double c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Área de um triângulo retângulo (base A, altura C): "+ ((a * c) / 2).ToString("F3", CultureInfo.InvariantCulture)+"\nÁrea de um círculo (raio C): "+ (3.14159 * Math.Pow(c, 2.0)).ToString("F3", CultureInfo.InvariantCulture)+"\nÁrea de um trapézio (bases A e B, altura C): "+ (((a + b) * c) /2).ToString("F3", CultureInfo.InvariantCulture)+"\nÁrea de um quadrado (lado B): "+ (b* b).ToString("F3", CultureInfo.InvariantCulture)+"\nÁrea de um retângulo (lados A e B): "+ (a*b).ToString("F3", CultureInfo.InvariantCulture));
         }
     }
 }
