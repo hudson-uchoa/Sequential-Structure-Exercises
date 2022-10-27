d67ed001f309b36bbabb6a6cefc419e7e8f2835c
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-10-27 20:20:11 -0300

    feat: adicionado Console.WriteLine nas linhas 12, 20, 27, 37, 48 e 66 para indicar qual exercicio está sendo executado.

--- ExerciciosEstruturaSequencial/Program.cs
+++ ExerciciosEstruturaSequencial/Program.cs
@@ -8,60 +8,62 @@ namespace ExeciciosEstruturaSequencial
         static void Main(string[] args)
         {
 
-            ////Exercicio 01
-            //Console.WriteLine("Digite um número inteiro:");
-            //int n1 = int.Parse(Console.ReadLine());
-            //Console.WriteLine("Digite outro número inteiro ao qual será somado ao anterior:");
-            //int n2 = int.Parse(Console.ReadLine());
-            //Console.WriteLine($"Resultado: {n1 + n2}");
+            //Exercicio 01
+            Console.WriteLine("Primeiro exercicio.");
+            Console.WriteLine("Digite um número inteiro:");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro número inteiro ao qual será somado ao anterior:");
+            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Resultado: {n1 + n2}");
 
-            ////Exercicio 02
-            //Console.WriteLine("Digite um raio de um circulo:");
-            //double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            //double pi = 3.14159;
-            //Console.WriteLine("Resultado: " + (pi * Math.Pow(raio, 2.0)).ToString("F4", CultureInfo.InvariantCulture));
+            //Exercicio 02
+            Console.WriteLine("Segundo exercicio.");
+            Console.WriteLine("Digite um raio de um circulo:");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double pi = 3.14159;
+            Console.WriteLine("Resultado: " + (pi * Math.Pow(raio, 2.0)).ToString("F4", CultureInfo.InvariantCulture));
 
-            ////Exercicio 03
+            //Exercicio 03
+            Console.WriteLine("Terceiro exercicio.");
+            Console.WriteLine("Digite 4 números inteiros em ordem sequencial na mesma linha (A, B, C e D):");
+            string[] nums = Console.ReadLine().Split(' ');
+            int A = int.Parse(nums[0]);
+            int B = int.Parse(nums[1]);
+            int C = int.Parse(nums[2]);
+            int D = int.Parse(nums[3]);
+            Console.WriteLine($"A diferença do produto de A e B pelo produto de C e D é: {(A * B) - (C * D)}");
 
-            //Console.WriteLine("Digite 4 números inteiros em ordem sequencial na mesma linha (A, B, C e D):");
-            //string[] nums = Console.ReadLine().Split(' ');
-            //int A = int.Parse(nums[0]);
-            //int B = int.Parse(nums[1]);
-            //int C = int.Parse(nums[2]);
-            //int D = int.Parse(nums[3]);
-            //Console.WriteLine($"A diferença do produto de A e B pelo produto de C e D é: {(A * B) - (C * D)}");
+            //Exercicio 04
+            Console.WriteLine("Quarto exercicio.");
+            Console.WriteLine("Qual o número do funcionario?");
+            int employee = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quantas horas o funcionario acima trabalhou?");
+            int hours = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quanto esse funcionario recebe por horas trabalhadas?");
+            double hourlyPaycheck = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            ////Exercicio 04
+            Console.WriteLine("Número do funcionario: " + employee + "\nSalário do funcionario: U$" + (hours * hourlyPaycheck).ToString("F2", CultureInfo.InvariantCulture));
 
-            //Console.WriteLine("Qual o número do funcionario?");
-            //int employee = int.Parse(Console.ReadLine());
-            //Console.WriteLine("Quantas horas o funcionario acima trabalhou?");
-            //int hours = int.Parse(Console.ReadLine());
-            //Console.WriteLine("Quanto esse funcionario recebe por horas trabalhadas?");
-            //double hourlyPaycheck = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Execicio 05
+            Console.WriteLine("Quinto exercicio.");
+            Console.WriteLine("Digite o código da primeira peça:");
+            int part1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite quantas desta peça irá precisar:");
+            int numOfPart1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Qual o valor unitário dessa peça?");
+            double priceOfPart1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            //Console.WriteLine("Número do funcionario: " + employee + "\nSalário do funcionario: U$" + (hours * hourlyPaycheck).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Digite o código da segunda peça:");
+            int part2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite quantas desta peça irá precisar:");
+            int numOfPart2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Qual o valor unitário dessa peça?");
+            double priceOfPart2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            ////Execicio 05
-
-            //Console.WriteLine("Digite o código da primeira peça:");
-            //int part1 = int.Parse(Console.ReadLine());
-            //Console.WriteLine("Digite quantas desta peça irá precisar:");
-            //int numOfPart1 = int.Parse(Console.ReadLine());
-            //Console.WriteLine("Qual o valor unitário dessa peça?");
-            //double priceOfPart1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            //Console.WriteLine("Digite o código da segunda peça:");
-            //int part2 = int.Parse(Console.ReadLine());
-            //Console.WriteLine("Digite quantas desta peça irá precisar:");
-            //int numOfPart2 = int.Parse(Console.ReadLine());
-            //Console.WriteLine("Qual o valor unitário dessa peça?");
-            //double priceOfPart2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            //Console.WriteLine("VALOR TOTAL A PAGAR: R$" + ((priceOfPart1 * numOfPart1) + (priceOfPart2 * numOfPart2)).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("VALOR TOTAL A PAGAR: R$" + ((priceOfPart1 * numOfPart1) + (priceOfPart2 * numOfPart2)).ToString("F2", CultureInfo.InvariantCulture));
 
             //Exercicio 06
-
+            Console.WriteLine("Sexto exercicio.");
             Console.WriteLine("Digite o valor de A (tem que ser um double):");
             double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Console.WriteLine("Digite o valor de B (tem que ser um double):");
